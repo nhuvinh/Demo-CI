36b423f768ae1bce8472430207837afa278d1175
Vinh Hoang <nhuvinh.hoang@gmail.com>
2026-07-14 16:10:59 +0700

    fail

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const tasksRouter = require('./routes/tasks');
+const tasksRouter = require("./routes/tasks");
 
 const app = express();
 
 app.use(express.json());
-app.use('/tasks', tasksRouter);
+app.use("/tasks", tasksRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ error: 'Not found' });
+  res.status(404).json({ error: "Not found" });
 });
 
 module.exports = app;
